9bb7e2d766fd12061f33c8abadd422c7c597c02a
KorEvgeniyfoGB <Bagz.bann4-l011a.Bann4@yandex.by>
2023-07-04 20:17:21 +0300

    new commit

--- task_006.py
+++ task_006.py
@@ -1,13 +1,15 @@
-a = int(input())
-b = 0
-c = 0
-while a > 999:
-    c += a % 10
-    a //= 10
-while a != 0:
-    b += a % 10
-    a //= 10
-if c == b:
-    print("YES")
+a = input()
+if len(a) % 2 == 0:
+    d = 0
+    e = 0
+    for i in range(len(a)):
+        if i >= (len(a) / 2):
+            d += int(a[i])
+        else:
+            e += int(a[i])
+    if d == e:
+        print("yes")
+    else:
+        print("no")
 else:
-    print("NO")
+    print("not lucky")
--- task_008.py
+++ task_008.py
@@ -1,7 +1,9 @@
 n = int(input())
 m = int(input())
 k = int(input())
-if k % m == 0:
+if k > m * n:
+    print("NO")
+elif k % m == 0 or k % n == 0:
     print("YES")
 else:
     print("NO")
